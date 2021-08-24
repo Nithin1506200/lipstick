2d8dee85e679715de095c6cd723d9a1eaed9a31c
Nithin N <57832822+Nithin1506200@users.noreply.github.com>
2021-08-24 20:21:20 +0530

    change 2

--- script.js
+++ script.js
@@ -1,44 +1,50 @@
-let video = document.getElementById('video');
-let model;
-let canvas =document.getElementById("canvas");
-tf.ENV.set("WEBGL_CPU_FORWARD", true);
-let ctx=canvas.getContext("2d");
-const setupCam = () =>{
-    navigator.mediaDevices.getUserMedia({
-        video: true,
-        audio:false,
-    }).then(stream => {
-        video.srcObject=stream;
-    });
-};
-const detectFaces = async () => {
-        const predictions = await model.estimateFaces({
-            input: video
+
+tf.ready().then(() => {
+    let video = document.getElementById('video');
+    let model;
+    let canvas =document.getElementById("canvas");
+    
+    let ctx=canvas.getContext("2d");
+    const setupCam = () =>{
+        navigator.mediaDevices.getUserMedia({
+            video: true,
+            audio:false,
+        }).then(stream => {
+            video.srcObject=stream;
         });
-        ctx.clearRect(0,0,canvas.width,canvas.height);
-if(predictions.length>0){
-    predictions.forEach((pred)=>{
-        ctx.beginPath();
-        ctx.lineWidth="4";
-        ctx.strokeStyle= "red";
-        ctx.rect(
-            pred.boundingBox.topLeft[0],
-            pred.boundingBox.topLeft[1],
-            pred.boundingBox.bottomRight[0]-pred.boundingBox.topLeft[0],
-            pred.boundingBox.bottomRight[1]-pred.boundingBox.topLeft[1] 
-        );
-        ctx.stroke();
-       // console.log(ctx);
-        ctx.fillStyle="green";
-        pred.scaledMesh.forEach(mesh =>{
-            ctx.fillRect(mesh[0],mesh[1],3,3)
+    };
+    const detectFaces = async () => {
+            const predictions = await model.estimateFaces({
+                input: video
+            });
+            ctx.clearRect(0,0,canvas.width,canvas.height);
+    if(predictions.length>0){
+        predictions.forEach((pred)=>{
+            ctx.beginPath();
+            ctx.lineWidth="4";
+            ctx.strokeStyle= "red";
+            ctx.rect(
+                pred.boundingBox.topLeft[0],
+                pred.boundingBox.topLeft[1],
+                pred.boundingBox.bottomRight[0]-pred.boundingBox.topLeft[0],
+                pred.boundingBox.bottomRight[1]-pred.boundingBox.topLeft[1] 
+            );
+            ctx.stroke();
+           // console.log(ctx);
+            ctx.fillStyle="green";
+            pred.scaledMesh.forEach(mesh =>{
+                ctx.fillRect(mesh[0],mesh[1],3,3)
+            });
         });
-    });
-}
-};
-setupCam();
-video.addEventListener("loadeddata",async () => {
-     model = await faceLandmarksDetection.load(
-        faceLandmarksDetection.SupportedPackages.mediapipeFacemesh);
-    setInterval(detectFaces,100);
-})
+    }
+    };
+    setupCam();
+    video.addEventListener("loadeddata",async () => {
+         model = await faceLandmarksDetection.load(
+            faceLandmarksDetection.SupportedPackages.mediapipeFacemesh);
+        setInterval(detectFaces,50);
+    })
+
+});
+
+
